38a47299ed9805b19b3fa7817ae83e8381980764
Dylan Smith <dylanjsmith.dev@gmail.com>
2025-10-23 00:33:34 -0400

    added homepage

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,37 @@
-import { HeadProvider } from "react-head";
-import AnimatedBackground from "./components/AnimatedBackground";
-import Navbar from "./components/NavBar";
-import Footer from "./components/Footer";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Contact from "./pages/Contact";
 import OurWork from "./pages/OurWork";
-import Pricing from "./pages/Pricing"; // <-- import Pricing page
-import ScrollToTop from "./components/ScrollToTop";
+import Pricing from "./pages/Pricing";
 import PrivacyPolicy from "./pages/PrivacyPolicy";
+import Navbar from "./components/NavBar";
+import Footer from "./components/Footer";
+import AnimatedBackground from "./components/AnimatedBackground";
+import ScrollToTop from "./components/ScrollToTop";
+import { HeadProvider } from "react-head"; // if using react-head for SEO
+
 function App() {
   return (
     <HeadProvider>
-    <Router>
-      {/* Animated background always behind everything */}
-      <AnimatedBackground />
+      <Router>
+        <AnimatedBackground />
+        <div className="content">
+          <Navbar />
+          <ScrollToTop />
 
-      {/* Content wrapper with z-index above background */}
-      <div className="content">
-        {/* Navbar always on top */}
-        <Navbar />
-         <ScrollToTop />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/services" element={<Services />} />
+            <Route path="/work" element={<OurWork />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          </Routes>
 
-        {/* Page content */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/work" element={<OurWork />} />
-          <Route path="/pricing" element={<Pricing />} /> {/* <-- add route */}
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-        </Routes>
-
-        {/* Footer always on bottom */}
-        <Footer />
-      </div>
-    </Router>
+          <Footer />
+        </div>
+      </Router>
     </HeadProvider>
   );
 }
